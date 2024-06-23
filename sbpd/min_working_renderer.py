c4462ba00a11d59ca784a66e338d16f4122cea80
leonardo santos <leohmcs@gmail.com>
2024-06-23 14:35:25 -0400

    properly set environment variables

--- sbpd/min_working_renderer.py
+++ sbpd/min_working_renderer.py
@@ -1,10 +1,11 @@
-import os, sys
-sys.path.append("/home/leo/git/Visual-Navigation-Release")
-os.system('export PYOPENGL_PLATFORM=egl')   # we have to explicitly choose the platform
+import sys, os
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).parent.parent))  # add Visual-Navigation-Release to PYTHONPATH
+os.environ['PYOPENGL_PLATFORM'] = 'egl' # we have to explicitly choose the opengl platform on linux
 
 import sbpd.sbpd_renderer as sbpdr
 import params.renderer_params as rp
-
 
 params = rp.create_params()
 renderer = sbpdr.SBPDRenderer(params)
